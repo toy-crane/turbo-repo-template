231c355cef4a4eed30c80f2e95fff14206f25420
toy-crane <alwaysfun2183@gmail.com>
2026-08-07 14:37:54 +0900

    fix: use Expo native module export

--- apps/mobile/src/utils/is-development-build.ts
+++ apps/mobile/src/utils/is-development-build.ts
@@ -1,4 +1,4 @@
-import { requireOptionalNativeModule } from "expo-modules-core";
+import { requireOptionalNativeModule } from "expo";
 
 interface DevelopmentBuildEvidence {
   hasDevLauncher: boolean;
